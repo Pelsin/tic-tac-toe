b9b71b277b0d6a52f1b758185403c965a0d247ea
Ian Vidales <ian.vidales@pingbull.se>
2014-03-02 16:49:21 +0100

    added draw and winner functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var TicTacToe = (function() {
 	"use strict";
 	var board = []; // Entire board, will be 2d array
+	var turn; // Number of turns
 	var boardHolder = document.getElementById('boardHolder'); // Element for the where we will append cells
 	var playerAnnouncer = document.getElementById('currentPlayer'); // Element for who the current player is
 
@@ -8,26 +9,82 @@ var TicTacToe = (function() {
 		config: {
 			players: 2, // Number or players
 			boardSize: 3, // Size of the board in cells
-			numberToWin: 3, // How many in a row to win
-			currentPlayer: 1, // Defines who can do the next move
-			playerColors: ['red', 'blue'] // Player colors
+			numbersToWin: 3, // How many in a row to win
+			currentPlayer: 1, // Define who can do the next move
+			playerColors: ['red', 'blue', 'green', 'orange', 'purple', 'black', 'silver'] // Player colors
 		},
 		start: function() {
+			turn = 0;
+			cleanBoard();
 			setPlayerAnnouncer();
-			generate2DBoard(this.config.boardSize);
+			generate2DBoard(TicTacToe.config.boardSize);
 		}
 	};
 
 	function playerMove() {
+		turn++;
 		this.style.backgroundColor = TicTacToe.config.playerColors[TicTacToe.config.currentPlayer-1];
 		board[this.dataset.row][this.dataset.col] = TicTacToe.config.currentPlayer;
 		this.removeEventListener("click", playerMove);
-		checkWinner();
+		if(checkWinner()) {
+			endGame("Player "+TicTacToe.config.currentPlayer+" is the winner!\nPlay again?");
+		} else if(turn == (TicTacToe.config.boardSize*TicTacToe.config.boardSize)) {
+			endGame("Draw!\nPlay again?");
+		}
 		nextPlayer();
 	}
 
 	function checkWinner() {
-		// TODO Check winner logic
+		for (var i=0;i<TicTacToe.config.boardSize;i++) {
+			for (var b=0;b<TicTacToe.config.boardSize;b++) {
+				if(board[i][b] == TicTacToe.config.currentPlayer) {
+					var right = '', downRight= '', down = '', downLeft = '';
+					var winningCombo = ''; // Holder for the winning combo, String(currentPlayer) * numbersToWin
+
+					for (var w=0;w<TicTacToe.config.numbersToWin;w++) {
+						winningCombo += TicTacToe.config.currentPlayer;
+						right += getCellValue(i,b+w);
+						downRight += getCellValue((i+w),b+w);
+						down += getCellValue(i+w,b);
+						downLeft += getCellValue(i+w,b-w);
+					}
+
+					var winnerPaths = {
+						right: right,
+						downRight: downRight,
+						down: down,
+						downLeft: downLeft
+					};
+
+					return checkWinnerPath(winningCombo, winnerPaths);
+				}
+			}
+		}
+	}
+
+	function checkWinnerPath(winningCombo, winnerPaths) {
+		for (var path in winnerPaths) {
+			if (winningCombo == winnerPaths[path]){
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	function getCellValue(row,col) {
+		if(typeof board[row] != 'undefined'){
+			if(typeof board[row][col] != 'undefined'){
+				return board[row][col];
+			}
+		}
+
+		return '0';
+	}
+
+	function endGame(msg){
+		alert(msg);
+		TicTacToe.start();
 	}
 
 	function nextPlayer() {
@@ -42,8 +99,14 @@ var TicTacToe = (function() {
 		playerAnnouncer.style.color = TicTacToe.config.playerColors[TicTacToe.config.currentPlayer-1];
 	}
 
+	function cleanBoard() {
+		board = [];
+		boardHolder.innerHTML = '';
+	}
+
 	function generate2DBoard() {
 		var mapRows = []; // Row holder data to generate mapResults
+
 		for (var i=0;i<TicTacToe.config.boardSize;i++) {
 			mapRows = [];
 			for (var b=0;b<TicTacToe.config.boardSize;b++) {
@@ -56,6 +119,7 @@ var TicTacToe = (function() {
 
 	function renderCell(row,col) {
 		var cell = document.createElement('div'); // Create cell
+
 		cell.className = 'boardCell';
 		cell.dataset.row = row;
 		cell.dataset.col = col;
